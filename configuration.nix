052d893591488747db6a585bbfafe0dfc707f92c
Julius de Bruijn <julius+github@nauk.io>
2021-02-15 21:57:09 +0100

    nix gc

--- configuration.nix
+++ configuration.nix
@@ -204,6 +204,14 @@
     };
   };
 
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than-7d";
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
