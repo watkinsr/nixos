3e89233c0836c317486c4cfe64d3445263f437f5
Julius de Bruijn <julius+github@nauk.io>
2021-02-15 20:58:20 +0100

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,219 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.grub.copyKernels = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+    supportedFilesystems = [ "zfs" ];
+  };
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
+    extraPackages = with pkgs; [
+      xwayland
+      swaylock
+      swayidle
+      wl-clipboard
+      mako # notification daemon
+      alacritty # Alacritty is the default terminal in the config
+      wofi
+      waybar
+      kanshi
+      i3blocks-gaps
+      i3status
+      wev
+    ];
+  };
+
+  programs.light.enable = true;
+  programs.qt5ct.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [
+    (self: super: {
+      wl-clipboard-x11 = super.stdenv.mkDerivation rec {
+      pname = "wl-clipboard-x11";
+      version = "5";
+    
+      src = super.fetchFromGitHub {
+        owner = "brunelli";
+        repo = "wl-clipboard-x11";
+        rev = "v${version}";
+        sha256 = "1y7jv7rps0sdzmm859wn2l8q4pg2x35smcrm7mbfxn5vrga0bslb";
+      };
+    
+      dontBuild = true;
+      dontConfigure = true;
+      propagatedBuildInputs = [ super.wl-clipboard ];
+      makeFlags = [ "PREFIX=$(out)" ];
+      };
+    
+      xsel = self.wl-clipboard-x11;
+      xclip = self.wl-clipboard-x11;
+    })
+  ];
+
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+
+  hardware.pulseaudio.enable = true;
+  hardware.opengl.driSupport = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pimeys = {
+     isNormalUser = true;
+     shell = pkgs.fish;
+     extraGroups = [
+       "wheel"
+       "video"
+       "networkmanager"
+       "docker"
+     ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     wget
+     neovim
+     firefox
+     emacs
+     wl-clipboard
+     brightnessctl
+     polkit_gnome
+     gtk-engine-murrine
+     gtk_engines
+     gsettings-desktop-schemas
+     lxappearance
+     fish
+     htop
+     thefuck
+     git
+     perl
+     sysstat
+     element-desktop
+     docker-compose
+     rustup
+     ncmpcpp
+     clang
+     zlib
+     binutils
+     openssl
+     automake
+     gnumake
+     rust-analyzer
+     ripgrep
+     fd
+  ];
+
+  environment.variables.MOZ_ENABLE_WAYLAND = "1";
+
+  environment.pathsToLink = [ "/libexec" ];
+
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      corefonts
+      inconsolata
+      ubuntu_font_family
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts
+      dina-font
+      proggyfonts
+      awesome
+      dejavu_fonts
+      nerdfonts
+      hack-font
+    ];
+  };
+
+  virtualisation.docker.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+ 
+  services = {
+    tlp = { enable = true; };
+    zfs = {
+      trim.enable = true;
+    };
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+}
+
