e809815223b67f38e4cf9678faa1dde853e6b9aa
Julius de Bruijn <julius@nauk.io>
2021-02-17 20:19:20 +0100

    Add DOOM EMACS

--- home.nix
+++ home.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }:
 
-{
+let
+  doom-emacs = pkgs.callPackage (builtins.fetchTarball {
+    url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
+  }) {
+    doomPrivateDir = ./home/doom.d;  # Directory containing your config.el init.el
+                                     # and packages.el files
+  };
+in {
+  home.packages = [ doom-emacs ];
+  home.file.".emacs.d/init.el".text = ''
+      (load "default.el")
+  '';
   programs = {
     git = {
       enable = true;
