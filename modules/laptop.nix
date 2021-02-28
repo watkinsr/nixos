7516eeec4a328891529fbd6ae4f7685095b5e16c
Julius de Bruijn <julius@nauk.io>
2021-02-28 12:39:11 +0100

    TLP settings

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -2,7 +2,21 @@
 
 {
   services = {
-    tlp = { enable = true; };
+    tlp = {
+      enable = true;
+      settings = {
+        TLP_ENABLE = 1;
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+        START_CHARGE_THRESH_BAT1 = 75;
+        STOP_CHARGE_THRESH_BAT1 = 80;
+        RESTORE_THRESHOLDS_ON_BAT = 1;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+        DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth wwan";
+        DEVICES_TO_ENABLE_ON_STARTUP = "wifi";
+      };
+    };
   };
 
   powerManagement = {
