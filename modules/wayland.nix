93679ea29e2b15e7bcddab24e916d2a218afe9e5
Julius de Bruijn <julius+github@nauk.io>
2021-03-28 22:22:06 +0200

    Move locking stuff to wayland module

--- modules/wayland.nix
+++ modules/wayland.nix
@@ -51,4 +51,27 @@
       polkit_gnome
     ];
   };
+
+  systemd.services.lock-before-sleeping = {
+    restartIfChanged = false;
+    unitConfig = {
+      Description = "Helper service to bind locker to sleep.target";
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2";
+      Type = "simple";
+      User = "pimeys";
+    };
+    before = [
+      "pre-sleep.service"
+    ];
+    wantedBy= [
+      "pre-sleep.service"
+    ];
+    environment = {
+      DISPLAY = ":1";
+      WAYLAND_DISPLAY = "wayland-1";
+      XDG_RUNTIME_DIR = "/run/user/1000";
+    };
+  };
 }
