efebf6d71a14d9490ee0125dd8e4919b8119008a
Julius de Bruijn <julius+github@nauk.io>
2021-10-16 18:31:54 +0200

    kak-lsp: inlay hints for rust-analyzer

--- home.nix
+++ home.nix
@@ -216,7 +216,15 @@ rec {
           {
             name = "WinSetOption";
             option = "filetype=rust";
-            commands = "hook window BufWritePre .* lsp-formatting-sync";
+            commands = ''
+              hook window BufWritePre .* lsp-formatting-sync
+              hook window -group rust-inlay-hints BufReload .* rust-analyzer-inlay-hints
+              hook window -group rust-inlay-hints NormalIdle .* rust-analyzer-inlay-hints
+              hook window -group rust-inlay-hints InsertIdle .* rust-analyzer-inlay-hints
+              hook -once -always window WinSetOption filetype=.* %{
+                remove-hooks window rust-inlay-hints
+              }
+            '';
           }
         ];
       };
