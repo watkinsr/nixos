c0a2086758a721a34d4bcbf5495d75685fa78d4b
Julius de Bruijn <julius+github@nauk.io>
2021-10-20 18:52:06 +0200

    neovim

--- home.nix
+++ home.nix
@@ -278,11 +278,115 @@ rec {
       enableFuzzySearch = true;
       keyScheme = "vim";
     };
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        deoplete-rust
+        deoplete-lsp
+
+        {
+          plugin = vim-autoformat;
+          config = ''
+            autocmd BufWrite * :Autoformat
+            autocmd Filetype rust setlocal omnifunc=v:lua.vim.lsp.omnifunc
+          '';
+        }
+        {
+          plugin = supertab;
+          config = ''
+            let g:SuperTabDefaultCompletionType = "<c-n>"
+          '';
+        }
+        {
+          plugin = ctrlp-vim;
+          config = ''
+            let g:ctrlp_working_path_mode = 'ra'
+            let g:ctrlp_user_command = 'rg %s --files --hidden --color=never --glob ""'
+          '';
+        }
+        {
+          plugin = deoplete-nvim;
+          config = ''
+            let g:deoplete#enable_at_startup = 1
+            call deoplete#custom#source('_', 'max_menu_width', 80)
+          '';
+        }
+        {
+          plugin = nvim-lspconfig;
+          config = ''
+              lua << EOF
+              local nvim_lsp = require('lspconfig')
+
+              -- Use an on_attach function to only map the following keys
+              -- after the language server attaches to the current buffer
+              local on_attach = function(client, bufnr)
+                local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+                local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+                -- Enable completion triggered by <c-x><c-o>
+                buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                -- Mappings.
+                local opts = { noremap=true, silent=true }
+
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+                buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+                buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+                buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+                buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+                buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+                buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+                buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+                buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+                buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+                buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+                buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+                buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+                buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+                buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+              end
+
+              -- Use a loop to conveniently call 'setup' on multiple servers and
+              -- map buffer local keybindings when the language server attaches
+              local servers = { 'rust_analyzer' }
+              for _, lsp in ipairs(servers) do
+                nvim_lsp[lsp].setup {
+                  on_attach = on_attach,
+                  flags = {
+                    debounce_text_changes = 150,
+                  }
+                }
+              end
+            EOF
+          '';
+        }
+        {
+          plugin = vim-airline;
+          config = "let g:airline_powerline_fonts = 1";
+        }
+        {
+          plugin = NeoSolarized;
+          config = ''
+            syntax enable
+            set background=dark
+            set termguicolors
+            colorscheme NeoSolarized
+          '';
+        }
+      ];
+    };
     fish = {
       enable = true;
       interactiveShellInit = ''
         set PATH ~/.cargo/bin ~/.local/bin $PATH
-        set EDITOR ${pkgs.kakoune}/bin/kak
+        set EDITOR nvim
       '';
       functions = {
         flakify = ''
