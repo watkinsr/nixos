d31ad0c2f0c1c1190c5888910bfd4efe6314497c
Julius de Bruijn <julius+github@nauk.io>
2021-10-21 20:49:21 +0200

    neovim: lsp config to a lua file

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 rec {
   xdg = {
     configFile = {
+      "nvim/lua".source = ./home/nvim/lua;
       "pictures".source = ./home/pictures;
       "wofi/style.css".source = ./home/wofi/style.css;
       "picom/config".source = ./home/picom/config;
@@ -322,17 +323,17 @@ rec {
           plugin = nvim-treesitter;
           config = ''
             lua <<EOF
-            require'nvim-treesitter.configs'.setup {
-              ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-              highlight = {
-                enable = true,              -- false will disable the whole extension
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-              },
-            }
+              require('nvim-treesitter.configs').setup {
+                ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+                highlight = {
+                  enable = true,              -- false will disable the whole extension
+                  -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                  -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                  -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                  -- Instead of true it can also be a list of languages
+                  additional_vim_regex_highlighting = false,
+                },
+              }
             EOF
           '';
         }
@@ -361,7 +362,6 @@ rec {
           config = ''
             let g:fzf_preview_window = ['right:50%', 'ctrl-/']
             let g:fzf_buffers_jump = 1
-
           '';
         }
         {
@@ -381,52 +381,7 @@ rec {
         {
           plugin = nvim-lspconfig;
           config = ''
-              lua << EOF
-              local nvim_lsp = require('lspconfig')
-
-              -- Use an on_attach function to only map the following keys
-              -- after the language server attaches to the current buffer
-              local on_attach = function(client, bufnr)
-                local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-                local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-                -- Enable completion triggered by <c-x><c-o>
-                buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-                -- Mappings.
-                local opts = { noremap=true, silent=true }
-
-                -- See `:help vim.lsp.*` for documentation on any of the below functions
-                buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-                buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-                buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-                buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-                buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-                buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-                buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-                buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-                buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-                buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-                buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-                buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-                buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-                buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-                buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-              end
-
-              -- Use a loop to conveniently call 'setup' on multiple servers and
-              -- map buffer local keybindings when the language server attaches
-              local servers = { 'rust_analyzer' }
-              for _, lsp in ipairs(servers) do
-                nvim_lsp[lsp].setup {
-                  on_attach = on_attach,
-                  flags = {
-                    debounce_text_changes = 150,
-                  }
-                }
-              end
-            EOF
+            lua require('nvim-lspconfig')
           '';
         }
         {
