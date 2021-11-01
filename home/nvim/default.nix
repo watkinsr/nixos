4eccf7d436b939a00f319e02f22e1f183501c4f7
Julius de Bruijn <julius+github@nauk.io>
2021-11-01 10:21:53 +0100

    neovim: set correct nvim-treesitter compiler

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -47,6 +47,7 @@
         plugin = nvim-treesitter;
         config = ''
           lua <<EOF
+            require 'nvim-treesitter.install'.compilers = { 'clang++'}
             require('nvim-treesitter.configs').setup {
               ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
               highlight = {
