84d225b99b0220d3ed7406cef1b43bf26d061ad1
Julius de Bruijn <julius+github@nauk.io>
2021-11-12 09:58:14 +0100

    Fix direnv config for flakes, update flakes

--- core/default.nix
+++ core/default.nix
@@ -36,10 +36,7 @@
     direnv = {
       enable = true;
       enableFishIntegration = true;
-      nix-direnv = {
-        enable = true;
-        enableFlakes = true;
-      };
+      nix-direnv = { enable = true; };
     };
     mcfly = {
       enable = true;
