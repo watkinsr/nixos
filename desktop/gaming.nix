237d9875b56205c7d955540c0505b83074d17d09
Julius de Bruijn <julius+github@nauk.io>
2021-11-17 12:07:16 +0100

    use ngg with radv

--- desktop/gaming.nix
+++ desktop/gaming.nix
@@ -1,7 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ lutris ];
+  environment = {
+    systemPackages = with pkgs; [ lutris vulkan-tools ];
+    variables.RADV_PERFTEST = "nggc";
+  };
 
   programs = { steam.enable = true; };
 
