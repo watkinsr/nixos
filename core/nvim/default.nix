f5be1ce51e67a4deceb223690ac44aa8851d6633
Julius de Bruijn <julius+github@nauk.io>
2021-11-24 14:46:32 +0100

    neovim: treesitter selection

--- core/nvim/default.nix
+++ core/nvim/default.nix
@@ -127,6 +127,15 @@
                   -- Instead of true it can also be a list of languages
                   additional_vim_regex_highlighting = false,
                 },
+                incremental_selection = {
+                  enable = true,
+                  keymaps = {
+                    init_selection = '<CR>',
+                    scope_incremental = '<CR>',
+                    node_incremental = '<TAB>',
+                    node_decremental = '<S-TAB>',
+                  },
+                },
               }
             EOF
           '';
